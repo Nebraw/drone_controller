aed5db79e52d6704c8d89ce5a05ab940d9ba01be
Nebraw <b.fredericmoreau@gmail.com>
2025-04-04 15:17:23 +0200

    [Update] first tentative

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src.controllers.drone_controller import DroneController
 from src.agents.agent import DroneAgent
 import sys
-import sys
+import time
 import platform
 import threading
 import cv2
@@ -43,52 +43,72 @@ if __name__ == "__main__":
     agent = DroneAgent(drone.tello)
 
     drone.connect()
-    drone.streamCamera()
 
-    #stop_event = threading.Event()
-    #video_thread = threading.Thread(target=startVideoStream, args=(drone.tello, stop_event))
-    #video_thread.start()
+    past_actions = []
+    reverse_action  = {
+        "move_forward": "move_back",
+        "move_left": "move_right",
+        "move_up": "move_down",
+        "rotate_cw": "rotate_ccw",
+        "move_back": "move_forward",
+        "move_right": "move_left",
+        "move_down": "move_up",
+        "rotate_ccw": "rotate_cw",
+    }
+
     print("Mode autonome activé !")       
     try:
         while True:
-            #frame = capture_frame(drone.tello)  # Récupère une image du drone
-            #action = agent.analyze_frame()  # Analyse et prédit une action
-            #agent.execute_action(action)  # Exécute l'action
-            frame_read = drone.tello.get_frame_read()
-            frame = frame_read.frame
-            cv2.imshow("Tello Video", frame)
-            cv2.waitKey(1)
+            print("Your desires are my order:")
             key = get_key().lower()
+
 
             match key:
                 case "w":
                     agent.execute_action("move_forward", 50)
+                    past_actions.append("move_forward")
                 case "s":
                     agent.execute_action("move_back", 50)
+                    past_actions.append("move_back")
                 case "a":
                     agent.execute_action("move_left", 50)
+                    past_actions.append("move_left")
                 case "d":
                     agent.execute_action("move_right", 50)
+                    past_actions.append("move_right")
                 case "r":
                     agent.execute_action("move_up", 50)
+                    past_actions.append("move_up")
                 case "f":
                     agent.execute_action("move_down", 50)
+                    past_actions.append("move_down")
                 case "q":
                     agent.execute_action("rotate_ccw", 45)
+                    past_actions.append("rotate_ccw")
                 case "e":
                     agent.execute_action("rotate_cw", 45)
+                    past_actions.append("rotate_cw")
                 case "t":
-                    agent.execute_action("takeoff")
+                    agent.execute_action("takeoff", 50)
                 case "l":
+                    drone.tello.land()
+                case "m":
+                    print(f"rewinding past actions: {past_actions}")
+                    while len(past_actions) == 0:
+                        back_action = reverse_action[past_actions.pop()]
+                        back_value = 45 if back_action[0] == "r" else 50
+                        print(f"rewind {back_action}, {back_value}")
+                        agent.execute_action(back_action, back_value)
+                        time.sleep(2)
                     drone.tello.land()
                 case "x":
                     drone.tello.land()
                     print("end program.")
                     break
                 case _: 
-                    print("Unknow", key)
+                    print("Unknown", key)
     finally:
         #stop_event.set()
         #video_thread.join()
-        drone.tello.streamoff()
-        cv2.destroyAllWindows()
+        #drone.tello.streamoff()
+        print("bye!")
